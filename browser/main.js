3ffa4652589dfab9cf7453500cc8a89f204a2a32
prakashnetoyed <prakash.netoyed@gmail.com>
2025-10-01 21:09:15 +0000

    Compiled code from Bitbucket Pipeline - Staging

--- browser/main.js
+++ browser/main.js
@@ -3253,6 +3253,18 @@ var AppComponent = class _AppComponent {
 })();
 
 // src/main.ts
+if (environment.production) {
+  const clarityScript = document.createElement("script");
+  clarityScript.type = "text/javascript";
+  clarityScript.innerHTML = `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tjjlmmrikb");
+  `;
+  document.head.appendChild(clarityScript);
+}
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
 /*! Bundled license information:
 
